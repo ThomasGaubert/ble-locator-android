6229966889031ab91ce649a8e383f5463676d90c
Thomas Gaubert <tpgaubert@gmail.com>
2015-06-09 17:29:26 -0700

    Disabled LeakCanary

--- mobile/src/main/java/com/tgaubert/blefinder/BLEFinderApplication.java
+++ mobile/src/main/java/com/tgaubert/blefinder/BLEFinderApplication.java
@@ -2,13 +2,11 @@ package com.tgaubert.blefinder;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
 public class BLEFinderApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        LeakCanary.install(this);
+        // LeakCanary.install(this);
     }
 }
