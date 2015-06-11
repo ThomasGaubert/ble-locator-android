a2b54b7b1055b72a11b2117476b388787b00edda
Thomas Gaubert <tpgaubert@gmail.com>
2015-06-11 12:56:36 -0700

    Added open source licenses dialog

--- mobile/src/main/java/com/tgaubert/blefinder/MainActivity.java
+++ mobile/src/main/java/com/tgaubert/blefinder/MainActivity.java
@@ -13,8 +13,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -95,6 +97,59 @@ public class MainActivity extends AppCompatActivity {
 
             if(pInfo != null)
                 ((TextView)dialog.findViewById(R.id.appVersion)).setText(pInfo.versionName);
+
+            dialog.findViewById(R.id.dialogLicenses).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+
+                    final Dialog licenseDialog = new Dialog(v.getContext());
+                    licenseDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                    licenseDialog.setContentView(R.layout.dialog_licenses);
+                    WindowManager.LayoutParams licenseParams = licenseDialog.getWindow().getAttributes();
+                    licenseParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+                    licenseDialog.getWindow().setAttributes(licenseParams);
+                    licenseDialog.show();
+
+                    String licenses = "<h3>android-beacon-library</h3>" +
+                            "<p>Copyright 2014 Radius Networks</p>\n" +
+                            "\n" +
+                            "   <p>Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+                            "   you may not use this file except in compliance with the License.\n" +
+                            "   You may obtain a copy of the License at</p>\n" +
+                            "\n" +
+                            "       <p>http://www.apache.org/licenses/LICENSE-2.0</p>\n" +
+                            "\n" +
+                            "   <p>Unless required by applicable law or agreed to in writing, software\n" +
+                            "   distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                            "   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                            "   See the License for the specific language governing permissions and\n" +
+                            "   limitations under the License.</p>" +
+                            "<h3>BottomSheet</h3>" +
+                            "<p>Copyright 2011, 2015 Kai Liao</p>\n" +
+                            "\n" +
+                            "<p>Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+                            "you may not use this file except in compliance with the License.\n" +
+                            "You may obtain a copy of the License at</p>\n" +
+                            "\n" +
+                            "   <p>http://www.apache.org/licenses/LICENSE-2.0</p>\n" +
+                            "\n" +
+                            "<p>Unless required by applicable law or agreed to in writing, software\n" +
+                            "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                            "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                            "See the License for the specific language governing permissions and\n" +
+                            "limitations under the License.</p>";
+
+                    ((TextView) licenseDialog.findViewById(R.id.dialogText)).setText(Html.fromHtml(licenses));
+
+                    licenseDialog.findViewById(R.id.licenseOk).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            licenseDialog.dismiss();
+                        }
+                    });
+                }
+            });
 
             return true;
         }
